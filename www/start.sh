8053dd3b5127e01dd13444fa25f987d65239d25f
philippe28 <phadc@hotmail.com>
2015-10-14 23:52:33 -0300

    tutorial

--- www/start.sh
+++ www/start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Diretorio e arquivo de log
 set -e
-LOGFILE=/home/philippe/projeto/api-django/Api-chat/logs/gunicorn.log
+LOGFILE=/home/philippe/projeto/api-django/Api-chat/www/logs/gunicorn.log
 LOGDIR=$(dirname $LOGFILE)
 # Numero de processo simultaneo, modifique de acordo com seu processador
 NUM_WORKERS=3
@@ -11,7 +11,7 @@ USER=myusr
 # Endereço local que o gunicorn irá rodar
 ADDRESS=127.0.0.1:8000
 # Ativando ambiente virtual e executando o gunicorn para este projeto
-source /home/philippe/projeto/lib/activate
+source /home/philippe/projeto/api-django/lib/bin
 cd /home/philippe/projeto/api-django/Api-chat/
 test -d $LOGDIR || mkdir -p $LOGDIR
-exec gunicorn -w $NUM_WORKERS --bind=$ADDRESS --user=$USER --log-level=debug --log-file=$LOGFILE 2>>$LOGFILE scrum.wsgi:application
+exec gunicorn -w $NUM_WORKERS --bind=$ADDRESS --user=$USER --log-level=debug --log-file=$LOGFILE 2>>$LOGFILE myproject.wsgi:application
