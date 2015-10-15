f74fde89b4cd73c09b61744969309f304f00f6be
philippe28 <philippe@philippe28.philippe28.g7.internal.cloudapp.net>
2015-10-15 00:54:20 +0000

    mudancas deploy

---
+++ start.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# Diretorio e arquivo de log
+set -e
+LOGFILE=/home/philippe/projeto/api-django/Api-chat/logs/gunicorn.log
+LOGDIR=$(dirname $LOGFILE)
+# Numero de processo simultaneo, modifique de acordo com seu processador
+NUM_WORKERS=3
+# Usuario/Grupo que vai rodar o gunicorn
+USER=myusr
+#GROUP=root
+# Endereço local que o gunicorn irá rodar
+ADDRESS=127.0.0.1:8000
+# Ativando ambiente virtual e executando o gunicorn para este projeto
+source /home/philippe/projeto/lib/activate
+cd /home/philippe/projeto/api-django/Api-chat/
+test -d $LOGDIR || mkdir -p $LOGDIR
+exec gunicorn -w $NUM_WORKERS --bind=$ADDRESS --user=$USER --log-level=debug --log-file=$LOGFILE 2>>$LOGFILE scrum.wsgi:application
